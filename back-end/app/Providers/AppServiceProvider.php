e496c42f4aa2114231bba584decb7d95bc556149
Sebastian Gaviria <juansebastiangaviriacardona@gmail.com>
2026-04-03 10:36:56 -0500

    Enforce HTTPS scheme in AppServiceProvider and define admin access gate

--- back-end/app/Providers/AppServiceProvider.php
+++ back-end/app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 use App\Models\User;
+use Illuminate\Support\Facades\URL;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -24,5 +26,8 @@ class AppServiceProvider extends ServiceProvider
         // Retorna true solo si el rol es exactamente 'admin'
         return $user->rol === 'admin';
     });
+
+    URL::forceScheme('https');
+
     }
 }
